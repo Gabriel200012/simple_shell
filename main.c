e0183660403bd4326946a9bf4b77333f9cd8e25d
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-07-26 09:24:34 +0100

    main.c

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ size_t len = 0;
 ssize_t nread;
 
 nread = getline(&line, &len, stdin);
-if (nread == -1) {
-    // Handle end of file condition
+if (nread == -1)
+{  
 free(line);
 exit(EXIT_SUCCESS);
 }
@@ -24,7 +24,7 @@ return line;
    main: entry
    create a function that splits a line into an arrays of argument
  */
-// A function that splits a line into an array of arguments
+
 char **split_line(char *line) {
 char **args = malloc(MAX_ARGS * sizeof(char *));
 char *token;
@@ -40,44 +40,53 @@ args[index] = NULL;
 return args;
 }
 
-// A function that executes a command with given arguments
-int execute(char **args) {
+int execute(char **args)
+{
 pid_t pid;
 int status;
 
-if (args[0] == NULL) {
-    // An empty command was entered
+if (args[0] == NULL)
+{
+   
 return 1;
 }
 
 pid = fork();
-if (pid == 0) {
-    // Child process
-if (execve(args[0], args, environ) == -1) {
-    // Handle executable not found error
+if (pid == 0)
+{
+    
+if (execve(args[0], args, environ) == -1)
+{
 perror(args[0]);
 }
 exit(EXIT_FAILURE);
-} else if (pid < 0) {
-    // Error forking
+}
+else if (pid < 0)
+{
+   
 perror("fork");
-} else {
-    // Parent process
-do {
+}
+else
+{
+do
+{
 waitpid(pid, &status, WUNTRACED);
-} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
+while (!WIFEXITED(status) && !WIFSIGNALED(status));
 }
 
 return 1;
 }
 
-// A function that loops to read, parse, and execute commands
-void loop(void) {
+
+void loop(void)
+{
 char *line;
 char **args;
 int status;
 
-do {
+do
+{
 printf("> ");
 line = read_line();
 args = split_line(line);
@@ -88,14 +97,13 @@ free(args);
 } while (status);
 }
 
-// The main function
-int main(int argc, char **argv) {
-  // Load config files, if any.
 
-  // Run command loop.
+int main(int argc, char **argv)
+{
+  
 loop();
 
-  // Perform any shutdown/cleanup.
+  
 
 return EXIT_SUCCESS;
 }
