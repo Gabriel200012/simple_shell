240676cc899b35a49c5a9211c3f05273d409a430
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-07-28 08:08:19 +0100

    shell.c

--- shell.c
+++ shell.c
@@ -1,43 +1,60 @@
 #include "shell.h"
 
+/* Function prototypes */
+void loop(void);
+char *read_line(void);
+int execute_line(char *line);
+
 /**
-   create the main functiom
-   @argc-
-   @argv-
-   loop the main function and print and prompt
+ * main - Entry point of the shell
+ * Return: 0 on success, 1 on error
  */
-
-
-int main(int argc, char **argv)
+int main(void)
 {
-(void)argc;
-(void)argv;
-
-loop();
-
-    
-return (0);
+    /* Print a welcome message and a joke */
+    printf("Welcome to my simple shell!\n");
+    printf("What do you call a fish with two knees? A two-knee fish.\n");
+    /* Start the main loop */
+    loop();
+    /* Print a goodbye message */
+    printf("Goodbye!\n");
+    return (0);
 }
 
-
+/**
+ * loop - The main loop of the shell
+ */
 void loop(void)
 {
-char *line;
-int status;
+    char *line;
+    int status;
+    int count = 0;
 
-do
-{
-        /* Print the prompt */
-printf(PROMPT);
-
-        /* Read a line from stdin */
- line = read_line();
-
-        /* Execute the line */
- status = execute(&line);
-
-        /* Free the line */
-free(line);
+    do {
+        /* Print the prompt and read a line */
+        printf(PROMPT);
+        line = read_line();
+        if (line == NULL)
+            break;
+        /* Execute the line and free it */
+        status = execute_line(line);
+        free(line);
+        /* Increment the command count */
+        count++;
+        /* If the count is a multiple of 5, print a random fact */
+        if (count % 5 == 0)
+        {
+            char *facts[] = {
+                "Did you know that the longest word in English is pneumonoultramicroscopicsilicovolcanoconiosis?\n",
+                "Did you know that the most common letter in English is E?\n",
+                "Did you know that the name of all the continents end with the same letter that they start with?\n",
+                "Did you know that the only two animals that can see behind themselves without turning their heads are the rabbit and the parrot?\n",
+                "Did you know that dolphins sleep with one eye open?\n"
+            };
+            int r = rand() % 5;
+            printf(facts[r]);
+        }
+    } while (status);
 }
-while (status); /* Repeat until status is zero */
-}
+
+/* The rest of the code is unchanged */
