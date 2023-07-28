3ee3055ad317e3de83d4bd8cb7e03162b9e179e3
Gabriel200012 <chukwuemekagabriel56@gmail.com>
2023-07-28 07:25:04 +0100

    betty.c

---
+++ betty.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers from 1 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    return (0);
+}
